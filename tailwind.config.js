e4c98d47b96295882cb404aed9fd8adc083c3243
yuu3 <yunosuke0802@icloud.com>
2020-07-09 03:25:33 +0900

    component class追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
+const plugin = require('tailwindcss/plugin')
+
 /*
 ** TailwindCSS Configuration File
 **
 ** Docs: https://tailwindcss.com/docs/configuration
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
+
 module.exports = {
   theme: {},
   variants: {},
-  plugins: [],
+  plugins: [
+    plugin(({ addComponents }) => {
+      const apply = {
+        '.centering': {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }
+      }
+      addComponents(apply)
+    })
+  ],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
