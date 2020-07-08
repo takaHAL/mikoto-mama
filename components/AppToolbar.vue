945b65ff0e8d5b5628c6d7ff55593359c3c30dc9
yuu3 <yunosuke0802@icloud.com>
2020-07-08 22:51:30 +0900

    add AppToolbar

---
+++ components/AppToolbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <ul class="flex">
+      <li
+        v-for="menu in menus"
+        :key="menu.context"
+      >
+        {{ menu.context }}
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type MenuType = {
+  menus: {
+    context: string;
+    to: string;
+  }[]
+}
+
+export default Vue.extend({
+  name: 'AppToolbar',
+  data (): MenuType {
+    return {
+      menus: [
+        { context: 'プロフィール', to: '' },
+        { context: '配信', to: '' },
+        { context: '音楽', to: '' },
+        { context: 'ラジオ', to: '' },
+        { context: 'リンク', to: '' }
+      ]
+    }
+  }
+})
+</script>
