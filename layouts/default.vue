a7ffc1329952ca83241e1eae35d5d0e6f8d2efb6
yuu3 <yunosuke0802@icloud.com>
2020-08-12 14:37:56 +0900

    rename from AppToolbar to TheHeader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="w-full">
-    <AppToolbar />
+    <TheHeader />
     <Nuxt class="w-full mx-auto min-h-screen py-16" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import AppToolbar from '@/components/AppToolbar.vue'
+import TheHeader from '@/components/TheHeader.vue'
 
 export default Vue.extend({
   name: 'Layout',
   components: {
-    AppToolbar
+    TheHeader
   }
 })
 </script>
